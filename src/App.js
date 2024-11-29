950cf8c5a40402638a98d878f1c63ad276b03e75
Rajdeep Hazra <rajdeep.hazra503@gmail.com>
2024-11-30 01:10:47 +0530

    login sexy
    login sexy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { useState } from 'react';
 import { signInWithPopup } from 'firebase/auth';
 import { auth, googleProvider } from './firebase/firebase'; // Import Firebase functions
-import './App.css';  // Importing the styles from App.css
+import './App.css'; // Importing the styles from App.css
 import AuthForm from './components/AuthForm';
+
 const App = () => {
   const [user, setUser] = useState(null);
 
-  
+  const handleGoogleSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, googleProvider);
+      setUser(result.user);
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
 
   return (
-    <div>
-      <h1>Welcome to the Task Management App</h1>
-      {!user ? (
-        <div>
-          <AuthForm/>
-        </div>
-      ) : (
-        <div>
-          <h2>Welcome, {user.displayName}</h2>
-          <p>You are signed in!</p>
-        </div>
-      )}
+    <div className="app-wrapper">
+      <div className="wrapper">
+        <h1>Welcome to the Task Management App</h1>
+        {!user ? (
+          <div className="auth-container">
+            <AuthForm onGoogleSignIn={handleGoogleSignIn} />
+          </div>
+        ) : (
+          <div className="user-info">
+            <h2>Welcome, {user.displayName}</h2>
+            <p>You are signed in!</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
